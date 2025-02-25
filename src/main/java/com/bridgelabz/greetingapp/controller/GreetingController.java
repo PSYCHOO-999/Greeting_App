2787d1cf49c8043b873406cc77f963fbfba41693
Ankit Raj <ankitrajkumar7667162310@gmail.com>
2025-02-25 14:57:16 +0530

    [Ankit] Added Code For UC5

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,34 +1,19 @@
 package com.bridgelabz.greetingapp.controller;
 
-import com.bridgelabz.greetingapp.dto.UserDTO;
-import com.bridgelabz.greetingapp.entity.GreetingEntity;
-import com.bridgelabz.greetingapp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import com.bridgelabz.greetingapp.service.GreetingService;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
-    private final GreetingService greetingService;
+    @Autowired
+    private GreetingService greetingService;
 
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
-
-    @GetMapping
-    public String getGreeting() {
-        return greetingService.getGreeting();
-    }
-
-    @PostMapping("/personalized")
-    public String getPersonalizedGreeting(@RequestBody UserDTO user) {
-        return greetingService.getPersonalizedGreeting(user);
-    }
-
-    @GetMapping("/all")
-    public List<GreetingEntity> getAllGreetings() {
-        return greetingService.getAllGreetings();
+    @GetMapping("/{id}")
+    public String getGreetingById(@PathVariable Long id) {
+        return greetingService.getGreetingById(id);
     }
 }
--- src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
@@ -1,40 +1,18 @@
 package com.bridgelabz.greetingapp.service;
 
-import com.bridgelabz.greetingapp.dto.UserDTO;
-import com.bridgelabz.greetingapp.entity.GreetingEntity;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.bridgelabz.greetingapp.repository.GreetingRepository;
 
 @Service
 public class GreetingService {
-    private final List<GreetingEntity> greetingList = new ArrayList<>();
+    @Autowired
+    private GreetingRepository greetingRepository;
 
-    public String getGreeting() {
-        GreetingEntity greeting = new GreetingEntity("Hello World");
-        greetingList.add(greeting);
-        return "{ \"message\": \"Hello World\" }";
-    }
-
-    public String getPersonalizedGreeting(UserDTO user) {
-        String message;
-        if (user.getFirstName() != null && user.getLastName() != null) {
-            message = "Hello, " + user.getFirstName() + " " + user.getLastName() + "!";
-        } else if (user.getFirstName() != null) {
-            message = "Hello, " + user.getFirstName() + "!";
-        } else if (user.getLastName() != null) {
-            message = "Hello, " + user.getLastName() + "!";
-        } else {
-            message = "Hello World";
-        }
-
-        GreetingEntity greeting = new GreetingEntity(message);
-        greetingList.add(greeting);
-        return "{ \"message\": \"" + message + "\" }";
-    }
-
-    public List<GreetingEntity> getAllGreetings() {
-        return greetingList;
+    public String getGreetingById(Long id) {
+        return greetingRepository.findGreetingById(id).orElse("Greeting not found");
     }
 }
